b5625e268495f62dbfc1d215e0b9fb2625dec0e2
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2024-02-28 11:04:08 +0100

    creation grille bouton

--- Picross_test.R
+++ Picross_test.R
@@ -19,7 +19,7 @@ creer_grille_picross <- function(taille, puzzle) {
     geom_text(aes(x = 0, y = taille:1, label = indices_lignes),
               hjust = 1.1, vjust = 0.5, size = 4, color = "black") +
     geom_text(aes(x = 1:taille, y = 0, label = indices_colonnes),
-              hjust = 0.5, vjust = -0.5, size = 4, color = "black", angle = 90) +
+              hjust = 0.5, vjust = -0.5, size = 4, color = "black") +
     scale_fill_manual(values = c("white", "black"),
                       name = "Case",
                       labels = c("Blanc", "Noir")) +
@@ -34,30 +34,60 @@ creer_grille_picross <- function(taille, puzzle) {
           plot.margin = unit(c(1, 1, 1, 1), "cm"))
 }
 
-# Définition de l'interface Shiny
 ui <- fluidPage(
-  titlePanel("Jeu Picross"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("taille", "Taille de la grille", min = 5, max = 15, value = 10)
-    ),
-    mainPanel(
-      plotOutput("grillePicross", width = "100%", height = "600px")
-    )
-  )
+  titlePanel("Picross Game"),
+
+  # Slider pour régler la taille de la grille
+  sliderInput("gridSize", "Taille de la Grille", min = 5, max = 10, value = 5),
+
+  # Grille de jeu Picross
+  uiOutput("picrossGrid")
 )
 
-# Définition du serveur Shiny
 server <- function(input, output) {
-  output$grillePicross <- renderPlot({
-    # Générer une matrice de Picross aléatoire pour l'exemple
-    puzzle <- matrix(sample(0:1, input$taille^2, replace = TRUE),
-                     nrow = input$taille, ncol = input$taille)
+  # Fonction pour créer la grille de jeu Picross
+  output$picrossGrid <- renderUI({
+    # Obtenir la taille de la grille depuis le slider
+    gridSize <- input$gridSize
 
-    # Créer et afficher la grille Picross avec indices
-    creer_grille_picross(input$taille, puzzle)
+    # Créer une matrice de boutons en fonction de la taille de la grille
+    picrossMatrix <- matrix(
+      data = lapply(1:(gridSize^2), function(i) {
+        actionButton(
+          inputId = paste0("cell", i),
+          label = "",
+          width = 50,  # Largeur fixe pour chaque bouton
+          height = 50  # Hauteur fixe pour chaque bouton
+        )
+      }),
+      nrow = gridSize,
+      byrow = TRUE
+    )
+
+    # Créer la grille de jeu Picross en utilisant la fonction do.call
+    picrossGrid <- do.call(tagList, lapply(1:gridSize, function(i) {
+      fluidRow(
+        column(width = 2, align = "center", textOutput(paste0("rowHint", i))),
+        column(width = 8, tagList(picrossMatrix[i, ])),
+        column(width = 2, align = "center", textOutput(paste0("colHint", i)))
+      )
+    }))
+
+    # Retourner la grille complète
+    picrossGrid
+  })
+
+  # Générer des indices pour les lignes et les colonnes
+  observe({
+    gridSize <- input$gridSize
+    output_rows <- lapply(1:gridSize, function(i) sample(0:1, gridSize, replace = TRUE))
+    output_cols <- lapply(1:gridSize, function(i) sample(0:1, gridSize, replace = TRUE))
+
+    for (i in 1:gridSize) {
+      output[[paste0("rowHint", i)]] <- renderText(paste(output_rows[[i]], collapse = " "))
+      output[[paste0("colHint", i)]] <- renderText(output_cols[[i]])
+    }
   })
 }
 
-# Lancer l'application Shiny
 shinyApp(ui, server)
